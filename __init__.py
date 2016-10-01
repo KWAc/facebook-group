0ec2925694c2136db0cd3fa5367c10594ffecd50
Nihal Mirpuri <nihal.mirpuri@gmail.com>
2016-10-02 01:18:18 +1000

    changed start.py to __init__.py for hosting flask server

--- __init__.py
+++ __init__.py
@@ -1,12 +1,7 @@
 import time
-import atexit
-from apscheduler.schedulers.background import BackgroundScheduler
-from apscheduler.triggers.interval import IntervalTrigger
 
 from flask import Flask, render_template
 import pymysql.cursors
-
-from scraper import Scraper
 
 from secrets import DB_USER, DB_PASSWORD, DB_NAME
 
@@ -40,7 +35,7 @@ def index():
     limit 10;""")
     posts = _sql("""select from_name, SUBSTRING(message, 1, 1000) as message, created_time as date, CONCAT("https://facebook.com/", group_id,"/posts/",post_id) as link
     from Post order by created_time desc limit 20""")
-    count = _sql('select count(*) as count from Post')
+    count = _sql('select count(*) as all_posts from Post')
     return render_template('index.html', top_posts=top_posts, posts=posts, count=count)
 
 @app.route('/posts')
@@ -50,14 +45,5 @@ def show_posts():
     count = _sql('select count(*) as count from Post')
     return render_template('show_posts.html', posts=posts, count=count)
 
-def start_scraper():
-    Scraper()
-
 if __name__ == "__main__":
-    scheduler = BackgroundScheduler()
-    scheduler.start()
-    scheduler.add_job(func=start_scraper, trigger=IntervalTrigger(seconds=60),id='scraper',name='Scrapes posts',replace_existing=True)
-
-    # shut down the scheduler when exiting the app
-    atexit.register(lambda: scheduler.shutdown())
     app.run(threaded=True, host="0.0.0.0")
